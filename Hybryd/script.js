c78872faed471d3bfb861fb6e0e9aafc83bbd4b8
Elizaveta Khudiakova <lisavaeta.khudiakova@gmail.com>
2014-10-21 17:28:23 +0900

    order

--- Hybryd/script.js
+++ Hybryd/script.js
@@ -1,10 +1,12 @@
+ $( document ).ready( function(){
+ 
  $(document).click(function() {
             // Simply hide the submenu on any click. Again, this is just a hacked
             // together menu/submenu structure to show the use of jQuery-menu-aim.
             $(".popover").css("display", "none");
             $("a.maintainHover").removeClass("maintainHover");
         });
-$( document ).ready( function(){
+ 
 var $menu = $(".navmenu--list");
 
         // jQuery-menu-aim: <meaningful part of the example>
@@ -15,13 +17,7 @@ var $menu = $(".navmenu--list");
             deactivate: deactivateSubmenu
         });
         });
-        // jQuery-menu-aim: </meaningful part of the example>
-
-        // jQuery-menu-aim: the following JS is used to show and hide the submenu
-        // contents. Again, this can be done in any number of ways. jQuery-menu-aim
-        // doesn't care how you do this, it just fires the activate and deactivate
-        // events at the right times so you know when to show and hide your submenus.
-        
+ 
        function activateSubmenu(row) {
             var $row = $(row),
 			$submenu=$row.children(".popover"); 
